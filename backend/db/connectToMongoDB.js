f73863440fbf5363ef7f21b0e25d2b22348b7cfe
Ritik Shukla <133401940+9653RITIK@users.noreply.github.com>
2025-04-20 23:09:18 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect("mongodb://127.0.0.1:27017");
+		await mongoose.connect(MONGO_DB_URI);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
